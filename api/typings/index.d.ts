9725cf9045b544436db050f80cdd3d47aa27042e
spurs0929 <briankang0929@gmail.com>
2023-01-18 09:45:52 +0800

    feat: add interface

---
+++ api/typings/index.d.ts
@@ -0,0 +1,38 @@
+import 'egg';
+
+interface IGetNewsListParams {
+  type: string,
+  pageNum: number,
+  count: number
+}
+
+interface IHttpGetParamas {
+  url: string,
+  data: any,
+  success: (data: any) => void,
+  fail: (err: any) => void
+}
+
+interface IPageData<T> {
+  data: Array<T> | null,
+  hasMore: boolean
+}
+
+interface INewsData {
+  uniqueKey: string,
+  title: string,
+  data: string,
+  category: string,
+  author_name: string,
+  url: string,
+  thumbnail_pic_s?: string,
+  thumbnail_pic_s02?: string,
+  thumbnail_pic_s03?: string
+}
+
+declare module 'egg' {
+  IHttpGetParamas,
+  IGetNewsListParams,
+  IPageData,
+  INewsData
+}
