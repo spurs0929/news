a5752e4921793fc39aa748861208956a9b6b4b7c
spurs0929 <briankang0929@gmail.com>
2023-01-18 12:32:01 +0800

    feat: import js css

--- news/src/main.ts
+++ news/src/main.ts
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './assets/js/common.js';
+import './assets/css/border.css';
+import './assets/css/resets.css';
+import './assets/css/iconfont.css';
+
 createApp(App).use(store).use(router).mount('#app')
