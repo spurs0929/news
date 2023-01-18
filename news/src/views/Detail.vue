2954718c46afe56c08f47e3671a1e9091f28cf96
spurs0929 <briankang0929@gmail.com>
2023-01-18 12:31:29 +0800

    feat: add Home, Detail, MyNews views

---
+++ news/src/views/Detail.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    詳情頁
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Detail'
+  }
+</script>
---
+++ news/src/views/MyNews.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    收藏列
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyNews'
+  }
+</script>
