73d720392a0f896d0fbe84c7535b5e7b78e2d286
spurs0929 <briankang0929@gmail.com>
2023-01-18 09:43:42 +0800

    feat: add api router

---
+++ api/app/router.ts
@@ -0,0 +1,7 @@
+import { Application } from 'egg';
+
+export default (app: Application) => {
+  const { controller, router } = app;
+
+  router.post('/api/news_list', controller.api.getNewsList);
+};
