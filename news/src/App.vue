3634557a60e4c73926d351700bad339f3c771601
spurs0929 <briankang0929@gmail.com>
2023-01-18 19:41:12 +0800

    feat: finished App.vue

--- news/src/App.vue
+++ news/src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <router-view/>
+  <div class="container">
+    <my-header />
+    <router-view v-slot="{ Component }"> 
+      <keep-alive include="Home">
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+  </div>
 </template>
 
 <script>
+  import MyHeader from './components/Header/index.vue';
+
   export default {
-    name: 'App'
+    name: 'App',
+    components: {
+      MyHeader
+    }
   }
 </script>
