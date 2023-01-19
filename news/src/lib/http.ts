db034a6b54d51d9c86372eaa18f14ae4468be1f0
spurs0929 <briankang0929@gmail.com>
2023-01-19 19:40:19 +0800

    feat: setting axios params

---
+++ news/src/lib/http.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+axios.defaults.baseURL = 'http://localhost:7001';
+
+// 設置請求攔截器
+axios.interceptors.request.use((config: AxiosRequestConfig) => {
+  return config;
+});
+
+// 設置響應攔截器
+axios.interceptors.response.use((res: AxiosResponse) => {
+  if(res.data.err === 1) {
+    return Promise.reject(res.data.data);
+  }
+
+  return res.data;
+}, (err) => {
+  return Promise.reject(err);
+});
+
+export default axios;
+
+
