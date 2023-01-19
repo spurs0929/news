14eadbc99459392f8a952c0ed9348ce1ea7817b1
spurs0929 <briankang0929@gmail.com>
2023-01-19 20:05:41 +0800

    feat: add enum NAV_TYPES, IPostData

--- news/src/typings/common.ts
+++ news/src/typings/common.ts
@@ -1,3 +1,4 @@
+// 路由對應的Header設置
 interface IHeaderInfo {
   name: string,
   title: string,
@@ -9,6 +10,29 @@ interface IHeaderInfo {
   rightPath: string
 }
 
+// 新聞類型的枚舉
+enum NAV_TYPES {
+  TOP = 'top',
+  SHEHUI = 'shehui',
+  GUONEI = 'guonei',
+  GUOJI = 'guoji',
+  YULE = 'yule',
+  TIYU = 'tiyu',
+  JUNSHI = 'junshi',
+  KEJI = 'keji',
+  CAIJING = 'caijing',
+  SHISHANG = 'shishang'
+}
+
+// 請求資料的參數類型
+interface IPostData {
+  type: NAV_TYPES,
+  pageNum: number,
+  count: number
+}
+
 export {
-  IHeaderInfo
+  IHeaderInfo,
+  NAV_TYPES,
+  IPostData
 }
