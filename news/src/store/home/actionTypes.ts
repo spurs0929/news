4445a9a3fe081c45a9f69ba5e36c9c4099073ef7
spurs0929 <briankang0929@gmail.com>
2023-01-19 21:22:41 +0800

    feat: add action type

---
+++ news/src/store/home/actionTypes.ts
@@ -0,0 +1,12 @@
+// 設置新聞資料
+const SET_NEWS_LIST: string = 'SET_NEWS_LIST';
+// 設置loading狀態
+const SET_LOADING: string = 'SET_LOADING';
+// 設置目前新聞類型
+const SET_CURRENT_TYPE: string = 'SET_CURRENT_TYPE';
+
+export {
+  SET_NEWS_LIST,
+  SET_LOADING,
+  SET_CURRENT_TYPE
+}
