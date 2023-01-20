f4f3439baa15210b0ee369163529815d7473c428
spurs0929 <briankang0929@gmail.com>
2023-01-20 20:56:50 +0800

    feat: define NavBar constants

---
+++ news/src/data/nav.ts
@@ -0,0 +1,46 @@
+import { INavBar } from "@/typings";
+
+const navData: INavBar[] = [
+  {
+    type: 'top',
+    title: '頭條'
+  },
+  {
+    type: 'shehui',
+    title: '社會'
+  },
+  {
+    type: 'guonei',
+    title: '國內'
+  },
+  {
+    type: 'guoji',
+    title: '國際'
+  },
+  {
+    type: 'yule',
+    title: '娛樂'
+  },
+  {
+    type: 'tiyu',
+    title: '體育'
+  },
+  {
+    type: 'junshi',
+    title: '軍事'
+  },
+  {
+    type: 'keji',
+    title: '科技'
+  },
+  {
+    type: 'caijing',
+    title: '財經'
+  },
+  {
+    type: 'shishang',
+    title: '時尚'
+  }
+];
+
+export default navData;
