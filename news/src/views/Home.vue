567c0e72ed3b87fda87968a125f044f6864a44d7
spurs0929 <briankang0929@gmail.com>
2023-01-20 21:52:00 +0800

    feat: add setCurrenType event

--- news/src/views/Home.vue
+++ news/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <nav-bar></nav-bar>
+    <nav-bar
+      @setCurrentType="setCurrentType"
+    >
+    </nav-bar>
   </div>
 </template>
 
@@ -9,6 +12,8 @@ import { defineComponent } from "vue"
 import { Store, useStore } from 'vuex';
 import { useNewsList } from '../compositions';
 import NavBar from '../components/NavBar/index.vue';
+import { NAV_TYPES } from "../typings";
+import { useNavType } from "../compositions/home";
 
   export default defineComponent({
     name: 'Home',
@@ -17,7 +22,15 @@ import NavBar from '../components/NavBar/index.vue';
     },
     setup() {
       const store: Store<any> = useStore();
+      // 返回新聞列表
       const newsList = useNewsList(store);
-    }
+      // 返回修改類型的方法 
+      const setCurrentType: (type: NAV_TYPES) => void = useNavType(store);  
+
+      return {
+        newsList,
+        setCurrentType,
+      }
+   }
   })
 </script>
