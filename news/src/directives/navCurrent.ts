82910f6fe46166fe31c9c660c1c157a36b4316f6
spurs0929 <briankang0929@gmail.com>
2023-01-20 21:48:40 +0800

    docs: add comment

--- news/src/directives/navCurrent.ts
+++ news/src/directives/navCurrent.ts
@@ -1,9 +1,11 @@
+// binding interface
 interface IBinding {
   value: IConfig,
   oldValue: IConfig | undefined,
   [key: string]: any
 }
 
+// binding.value interface
 interface IConfig {
   activeClass: string,
   className: string,
@@ -11,15 +13,19 @@ interface IConfig {
 }
 
 export default {
+  // 掛載後
   mounted(el: HTMLElement, { value }: IBinding ) {
     const { activeClass, className, curIdx } = value;
     const oNavItems: HTMLCollection = el.getElementsByClassName(className);
 
+    // 在目前元素上新增active樣式
     oNavItems[curIdx].className += ` ${activeClass}`
   },
+  // 更新後
   updated(el: HTMLElement, { value, oldValue }: IBinding) {
     const { activeClass, className, curIdx } = value;
     const oNavItems: HTMLCollection = el.getElementsByClassName(className);
+    // last index
     const oldCurIdx: number = oldValue!.curIdx;
 
     oNavItems[oldCurIdx].className = `${className}`;
