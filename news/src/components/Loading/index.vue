96fac26169130b9aa7621f36d6f994bd288645be
spurs0929 <briankang0929@gmail.com>
2023-01-22 16:18:27 +0800

    feat: add loading component

---
+++ news/src/components/Loading/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="loading">加載中...</div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
+
+<style lang="scss" scoped>
+  .loading {
+    height: .38rem;
+    line-height: .38rem;
+    text-align: center;
+    font-size: .14rem;
+    color: #999;
+  }
+</style>
