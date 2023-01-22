fa2532caca42db39a6cb5aae7c82a8c7dcf5cafd
spurs0929 <briankang0929@gmail.com>
2023-01-22 16:19:09 +0800

    feat: add no-more component

---
+++ news/src/components/NoMore/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="no-more">沒有更多資料了...</div>
+</template>
+
+<script>
+export default {
+  name: 'NoMore'
+}
+</script>
+
+<style lang="scss" scoped>
+  .no-more {
+    height: .38rem;
+    line-height: .38rem;
+    text-align: center;
+    font-size: .14rem;
+    color: #999;
+  }
+</style>
