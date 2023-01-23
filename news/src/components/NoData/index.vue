1cb97c10d49e9786dc7c41b457d6aae8bb2b617c
spurs0929 <briankang0929@gmail.com>
2023-01-23 23:15:25 +0800

    feat: add NoData component

---
+++ news/src/components/NoData/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="no-data">
+    - 沒有收藏資料 -
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoData'
+}
+</script>
+
+<style lang="scss" scoped>
+  .no-data {
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    font-size: .16rem;
+    color: #999;
+  }
+</style>
