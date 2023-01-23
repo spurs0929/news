7b079117c0f1da444f24434a5d8c6ace1aeeaa3d
spurs0929 <briankang0929@gmail.com>
2023-01-23 23:16:50 +0800

    feat: add useNewsFollow, useFollowedCheck

--- news/src/compositions/detail.ts
+++ news/src/compositions/detail.ts
@@ -1,10 +1,11 @@
 import { IGlobalState } from "@/store";
 import { SET_CURRENT_NEWS } from "@/store/detail/actionTypes";
 import { INewsInfo } from "@/typings";
+import { computed } from "vue";
 import { RouteLocationNormalizedLoaded } from "vue-router";
 import { Store } from "vuex";
 
-// 獲取目前新聞訊息
+// 收藏新聞
 function useDetailInfo(store: Store<IGlobalState>, route: RouteLocationNormalizedLoaded): INewsInfo | undefined {
   // 新聞的唯一key
   const uniquekey: string = route.params.uniquekey as string;
@@ -19,15 +20,72 @@ function useDetailInfo(store: Store<IGlobalState>, route: RouteLocationNormalize
    */ 
   const newsData: INewsInfo[] = pageFrom === 'MyNews'
         ? JSON.parse(localStorage.getItem('newsList') || '[]')
-        : store.state.home.newsList.news;
-     
-  const newsInfo: INewsInfo | undefined = newsData.find((item: INewsInfo) => item.uniqueKey === uniquekey);
+        : store.state.home.newsList.news;   
+  const newsInfo: INewsInfo | undefined = newsData.find((item: INewsInfo) => item.uniquekey === uniquekey);
   // 設置目前新聞
   store.dispatch(`detail/${SET_CURRENT_NEWS}`, newsInfo);
   
   return newsInfo;
 }
 
+function useNewsFollow(store: Store<IGlobalState>, callback: (status: boolean) => void): void {
+  console.log(store);
+  const currentNews: INewsInfo = computed(() => store.state.detail.currentNews).value;
+
+  let newsList: INewsInfo[] = JSON.parse(localStorage.getItem('newsList') || '[]');
+  let followStatus: boolean = false;
+
+  // 有收藏的新聞列表
+  if(newsList.length) {
+    // 收藏的新聞是否跟目前想收藏的新聞相同
+    const isExist: INewsInfo | undefined = newsList.find((item: INewsInfo) => item.uniquekey === currentNews.uniquekey);
+    if(isExist) {
+      newsList = newsList.filter((item: INewsInfo) => item.uniquekey !== currentNews.uniquekey);
+      followStatus = false;
+    } else {
+      newsList.push(currentNews);
+      // 加入收藏
+      followStatus = true; 
+    }
+  } else {
+    // 將目前新聞加入新聞列表
+    newsList.push(currentNews);
+    // 加入收藏
+    followStatus = true;
+  }
+
+  localStorage.setItem('newsList', JSON.stringify(newsList));
+  callback(followStatus);
+}
+
+function useFollowedCheck(route: RouteLocationNormalizedLoaded, callback: (status: boolean) => void) {
+  const uniquekey = route.params.uniquekey;
+  const pageFrom = route.params.from;
+
+  // 是否為收藏的新聞
+  if(pageFrom === 'MyNews') {
+    callback(true);
+    return;
+  }
+
+  // 確認localStorage有無新聞列表
+  const newsList: INewsInfo[] = JSON.parse(localStorage.getItem('newsList') || '[]');
+
+  // 沒有新聞列表
+  if(!newsList.length) {
+    // 沒有收藏
+    callback(false);
+    return;
+  }
+  
+  // 檢查當前的uniquekey是否存在newsList中的某一條新聞
+  const isExist: INewsInfo | undefined = newsList.find((item: INewsInfo) => item.uniquekey === uniquekey);
+  // 
+  callback(isExist ? true: false);
+}
+
 export {
-  useDetailInfo
+  useDetailInfo,
+  useFollowedCheck,
+  useNewsFollow
 }
