2e7bcfd58494b252a5d5fc09de2fbe1402d35dcf
spurs0929 <briankang0929@gmail.com>
2023-01-23 23:22:26 +0800

    feat: add MyNews component

--- news/src/views/MyNews.vue
+++ news/src/views/MyNews.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="container">
-    收藏列
+    <news-list
+      :newsData="followedList"
+      :top="44"
+      v-if="followedList"
+    />
+    <no-data v-if="!followedList" />
   </div>
 </template>
 
-<script>
-  export default {
-    name: 'MyNews'
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useFollowedList } from '../compositions';
+import { INewsInfo } from '../typings';
+
+// 組件
+import NewsList from '../components/NewsList/index.vue';
+import NoData from '../components/NoData/index.vue';
+
+export default defineComponent({
+  name: 'MyNews',
+  components: {
+    NewsList,
+    NoData
+  },
+  setup() {
+    const followedList: INewsInfo[] | null = useFollowedList();
+
+    return {
+      followedList
+    }
   }
+})
 </script>
